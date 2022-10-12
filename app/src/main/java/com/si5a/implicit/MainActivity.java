7ec411a6322b647b9ec6486bbf37764c6b365198
clarameyhazlinda <clarameyhazlinda@mhs.mdp.ac.id>
2022-10-12 17:07:15 +0700

    Update Project

--- app/src/main/java/com/si5a/implicit/MainActivity.java
+++ app/src/main/java/com/si5a/implicit/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
 
             }
         });
